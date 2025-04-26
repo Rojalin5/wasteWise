28725fd6e44b4ddd87de0ab64f72b36115575267
Rojalin Barik <rojalinbarik2005@gmail.com>
2025-04-26 22:59:00 +0530

    Add fade-in animations to header and sections; implement Intersection Observer for scroll-triggered effects

---
+++ script.js
@@ -0,0 +1,16 @@
+const elementsToAnimate = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-viewport');
+          observer.unobserve(entry.target); 
+        }
+      });
+    }, {
+      threshold: 0.30 
+    });
+
+    elementsToAnimate.forEach((element) => {
+      observer.observe(element);
+    });
